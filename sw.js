571a67c1affb3d5edc8f3c5658094f7019d3fad9
peppyfashion775 <peppyfashionbd@gmail.com>
2026-08-04 17:12:40 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,44 @@
-const CACHE_NAME = "peppy-fashion-v1";
+const CACHE_NAME = "peppy-fashion-v2";
 
 const urlsToCache = [
   "/peppy-fashion-v2/",
   "/peppy-fashion-v2/index.html",
   "/peppy-fashion-v2/shop.html",
-  "/peppy-fashion-v2/css/style.css",
-  "/peppy-fashion-v2/js/script.js",
-  "/peppy-fashion-v2/js/products.js",
+  "/peppy-fashion-v2/cart.html",
+  "/peppy-fashion-v2/checkout.html",
+  "/peppy-fashion-v2/contact.html",
+  "/peppy-fashion-v2/offline.html",
+
+  "/peppy-fashion-v2/manifest.json",
+
+  "/peppy-fashion-v2/assets/css/style.css",
+
+  "/peppy-fashion-v2/assets/js/script.js",
+  "/peppy-fashion-v2/assets/js/products.js",
+  "/peppy-fashion-v2/assets/js/cart.js",
+  "/peppy-fashion-v2/assets/js/pwa.js",
+
   "/peppy-fashion-v2/icons/icon-192.png",
-  "/peppy-fashion-v2/icons/icon-512.png"
+  "/peppy-fashion-v2/icons/icon-512.png",
+
+  "/peppy-fashion-v2/assets/images/logo/logo.png",
+  "/peppy-fashion-v2/assets/images/banners/banner.jpg"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+
+    caches.open(CACHE_NAME).then(cache => {
+
+      return cache.addAll(urlsToCache);
+
+    })
+
   );
+
+  self.skipWaiting();
+
 });
 
 self.addEventListener("fetch", event => {
@@ -23,4 +47,32 @@ self.addEventListener("fetch", event => {
       return response || fetch(event.request);
     })
   );
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
 });
